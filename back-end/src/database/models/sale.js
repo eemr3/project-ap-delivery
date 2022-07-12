156a7d32ea008b66c4b98aba2e272ae0e03f94bd
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-12 18:18:10 -0300

    feat(models): implementado as models do banco de dados

---
+++ back-end/src/database/models/sale.js
@@ -0,0 +1,55 @@
+// 'use strict';
+// const { Model } = require('sequelize');
+
+// module.exports = (sequelize, DataTypes) => {
+//   class sale extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       sale.belongsTo(models.user)
+//     }
+//   }
+//   sale.init({
+//     userId: DataTypes.INTEGER,
+//     sellerId: DataTypes.INTEGER,
+//     totalPrice: DataTypes.DECIMAL,
+//     deliveryAddress: DataTypes.STRING,
+//     deliveryNumber: DataTypes.STRING,
+//     saleDate: DataTypes.DATETIME,
+//     status: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     underscored: true,
+//     modelName: 'sales',
+//     timestamps: false,
+//   });
+//   return sale;
+// };
+
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    userId: DataTypes.INTEGER,
+    sellerId: DataTypes.INTEGER,
+    totalPrice: DataTypes.DECIMAL,
+    deliveryAddress: DataTypes.STRING,
+    deliveryNumber: DataTypes.STRING,
+    saleDate: DataTypes.DATEONLY,
+    status: DataTypes.STRING
+  }, {
+    underscored: true,
+    modelName: 'users',
+    timestamps: false
+  });
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, {
+      as: 'users',
+      foreignKey: 'id',
+    });
+  };
+
+  return Sale;
+};
---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,48 @@
+// const {
+//   Model,
+// } = require('sequelize');
+
+// module.exports = (sequelize, DataTypes) => {
+//   class user extends Model {}
+  
+//   user.init({
+//     name: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//     password: DataTypes.STRING,
+//     role: DataTypes.STRING,
+//   }, {
+//     sequelize,
+//     modelName: 'users',
+//     timestamps: false,
+//   });
+
+//   user.hasMany(sales, { foreignKey: 'user_id', as: 'user' });
+//   user.hasMany(sales, { foreignKey: 'seller_id', as: 'seller' });
+
+//   sale.belongsTo(user, { foreignKey: 'user_id', as: 'user' });
+//   sale.belongsTo(user, { foreignKey: 'seller_id', as: 'seller' });
+
+//   return user;
+// };
+
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    modelName: 'users',
+    timestamps: false
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.sale, {
+      as: 'sales',
+      foreignKey: 'id',
+    });
+  };
+
+  return User;
+};
