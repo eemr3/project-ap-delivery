f6969df5856aa730b982748205e209e12b209443
eemr3 <eemr3@yahoo.com.br>
2022-07-13 18:50:30 -0300

    feat(token.js): cria funçoes de gerar token e decodificar token

---
+++ back-end/src/auth/token.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs/promises');
+
+const createdToken = async (user) => {
+  const secret = await fs.readFile('./jwt.evaluation.key', 'utf-8');
+  const token = jwt.sign(user, secret, {
+    expiresIn: '30d',
+    algorithm: 'HS256',
+  });
+   
+  return token;
+};
+
+const decodedToken = (token) => {
+  try {
+    const dedocoded = jwt.verify(token, fs.readFile('./jwt.evaluation.key', 'utf-8'));
+
+    return dedocoded;
+  } catch (error) {
+    return false;
+  }
+};
+module.exports = {
+  createdToken,
+  decodedToken,
+};
