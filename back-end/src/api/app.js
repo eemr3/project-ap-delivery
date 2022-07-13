1d27271ff7c57dae9970bdc7ddb96d7887d122f5
eemr3 <eemr3@yahoo.com.br>
2022-07-13 18:43:53 -0300

    feat(app.js): adiciona o middleware de error

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const routes = require('../routes');
+const handleError = require('../middlewares/handleError');
 
 const app = express();
+app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use('/login', routes.LoginRouter);
 
+app.use(handleError);
 module.exports = app;
