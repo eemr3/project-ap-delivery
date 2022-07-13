972d6a4392a23dda90ca705528ce26d025d19f9e
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-12 22:13:25 -0300

    test(login): implementado teste de integração da rota de login

--- back-end/src/database/seeders/20220712205638-users.js
+++ back-end/src/database/seeders/20220712205638-users.js
@@ -14,19 +14,19 @@ module.exports = {
         {
           name: "Peter Pan",
           email: "peterpan@test.com",
-          password: "123456",
+          password: "1234567",
           role: "customer",
         },
         {
           name: "Dona Tereza",
           email: "donatereza@test.com",
-          password: "123456",
+          password: "12345678",
           role: "administrator",
         },
         {
           name: "Zé das Coves",
           email: "zedascoves@test.com",
-          password: "123456",
+          password: "123456789",
           role: "seller",
         },
       ],
