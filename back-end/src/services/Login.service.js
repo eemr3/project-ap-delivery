821e4975f103af3de68a23fe36fca13cc1f10357
eemr3 <eemr3@yahoo.com.br>
2022-07-14 17:23:59 -0300

    fix(Login.service.js): corige erro de lint

--- back-end/src/services/Login.service.js
+++ back-end/src/services/Login.service.js
@@ -1,4 +1,4 @@
-const md5 = require('md5')
+const md5 = require('md5');
 const { createdToken } = require('../auth/token');
 const { User } = require('../database/models');
 const ErrorBase = require('../util/errorBase');
@@ -10,17 +10,19 @@ const signIn = async (emailUser, password) => {
 
   if (!user) throw ErrorBase(404, 'E-mail or password incorrect');
 
-  const isPwd = md5(password)
-  if (isPwd !== user.password ) throw ErrorBase(409, 'E-mail or password incorrect');
-  
+  const isPwd = md5(password);
+  if (isPwd !== user.password) {
+    throw ErrorBase(409, 'E-mail or password incorrect');
+  }
+
   const { name, email, role } = user;
-   const hasToken = await createdToken({ name, email, role });
+  const hasToken = await createdToken({ name, email, role });
 
   return {
     user: {
       name,
       email,
-      role,    
+      role,
     },
     hasToken,
   };
