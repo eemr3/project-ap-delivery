f5e87425d6343031536a944deafad055a522973f
JVSNG <joaovitorspala.js@gmail.com>
2022-07-14 20:14:04 -0300

    feat(service): Adciona serfice de registro com função de create

---
+++ back-end/src/services/Register.service.js
@@ -0,0 +1,22 @@
+const { User } = require('../database/models');
+const md5 = require('md5');
+const ErrorBase = require('../util/errorBase');
+
+const createRegister = async (userInfo) => {
+  const { password } = userInfo;
+
+  const encryptedPassword = md5(password);
+
+  const createdUser = await User.create({
+    ...userInfo,
+    password: encryptedPassword,
+  });
+
+  if (!createdUser) throw ErrorBase(500, 'Internal server error');
+
+  return createdUser;
+}
+
+module.exports = {
+  createRegister,
+};
