832c4c88b3996898022fd82a373f1c3b5dfe5e21
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-14 23:29:15 -0300

    feat(Products.Routes.js): Adcionado a rota de produtos e seus métodos

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,7 @@
 const LoginRouter = require('./LoginRoutes/Login.routes');
+const ProductRouter = require('./ProductRoutes/Product.routes');
 
 module.exports = {
   LoginRouter,
+  ProductRouter,
 };
