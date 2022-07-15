d35ba05db96c8dc832675926133d0933d1950a34
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-14 23:30:53 -0300

    feat(Products.service.js): adiciona o services de produtos e seus métodos

---
+++ back-end/src/services/Product.service.js
@@ -0,0 +1,62 @@
+const { Product } = require('../database/models');
+const ErrorBase = require('../util/errorBase');
+const { notFound, conflict } = require('../util/messageError');
+
+const getByNameProduct = async (name) => {
+  const product = Product.findOne({ where: { name } });
+  if (!product) throw ErrorBase(notFound.status, notFound.message);
+  return product;
+};
+
+const createProduct = async (body) => {
+  const productExist = await getByNameProduct(body.name);
+  
+  if (productExist) throw ErrorBase(conflict.status, conflict.message); 
+  
+  const product = await Product.create(body);
+
+  return product;
+};
+
+const getAllProducts = async () => {
+  const products = await Product.findAll();
+  
+  return products;
+};
+
+const getByIdProduct = async (id) => {
+  const product = await Product.findOne({ where: { id } });
+  
+  if (!product) throw ErrorBase(notFound.status, notFound.message);
+
+  return product;
+};
+
+const updateProduct = async (id, body) => {
+  const productExist = await getByIdProduct(id);
+
+  if (!productExist) throw ErrorBase(notFound.status, notFound.message);
+
+  const ok = await Product.update(body, { where: { id } });
+
+  if (!ok) throw ErrorBase(500, 'Internal error');
+};
+
+const deletProduct = async (id) => {
+  const productExist = await getByIdProduct(id);
+
+  if (!productExist) throw ErrorBase(notFound.status, notFound.message);
+
+  const ok = await Product.destroy({ where: { id } });
+
+  if (!ok) throw ErrorBase(500, 'Internal error');
+}; 
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getByNameProduct,
+  getByIdProduct,
+  updateProduct,
+  deletProduct,
+};
