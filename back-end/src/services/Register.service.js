eacdd66b5f0217a1f8206e242ba3a99689bd8a71
JVSNG <joaovitorspala.js@gmail.com>
2022-07-15 15:08:12 -0300

    fix(Register Route): Adiciona geração de token na service de Register e ajusta alguns erros do joi

--- back-end/src/services/Register.service.js
+++ back-end/src/services/Register.service.js
@@ -1,20 +1,31 @@
 const md5 = require('md5');
 const { User } = require('../database/models');
 const ErrorBase = require('../util/errorBase');
+const { createdToken } = require('../auth/token');
 
 const createRegister = async (userInfo) => {
-  const { password } = userInfo;
+  const { password, email, name } = userInfo;
 
+  const user = {
+    name,
+    email,
+    role: 'customer',
+  }
+
+  const hasToken = await createdToken(user);
   const encryptedPassword = md5(password);
 
   const createdUser = await User.create({
-    ...userInfo,
+    ...user,
     password: encryptedPassword,
   });
 
   if (!createdUser) throw ErrorBase(500, 'Internal server error');
 
-  return createdUser;
+  return {
+    user,
+    hasToken,
+  };
 };
 
 module.exports = {
