b34141681bb8a8b77836e50d0970ff4d04bd30ce
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-15 21:19:14 -0300

    chore(Docker): configuração de Docker

--- apps_install.sh
+++ apps_install.sh
@@ -1,11 +1,21 @@
+printf "\n> Instalando a Aplicação\n"
+AppFolder="./"
+cacheFolderApp="/tmp/app-cache"
+rm -rf $cacheFolderApp
+npm_config_loglevel=silent npm i --prefix ${AppFolder} --cache $cacheFolderApp
+
 printf "\n> Instalando o front-end\n"
-frontFolder="./frontend"
+frontFolder="./front-end"
 cacheFolderFront="/tmp/frontend-cache"
 rm -rf $cacheFolderFront
 npm_config_loglevel=silent npm i --prefix ${frontFolder} --cache $cacheFolderFront
 
 printf "\n> Instalando o back-end\n"
-backFolder="./backend"
+backFolder="./back-end"
 cacheFolderBack="/tmp/backend-cache"
 rm -rf $cacheFolderBack
 npm_config_loglevel=silent npm i --prefix ${backFolder} --cache $cacheFolderBack
+
+printf "\n> Configurando o docker\n"
+dockerFolder="./"
+npm_config_loglevel=silent npm run compose:up --prefix ${dockerFolder}
