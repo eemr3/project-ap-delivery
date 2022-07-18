6a15c6d816ae654185bee157344e4d96fca8c181
JVSNG <joaovitorspala.js@gmail.com>
2022-07-18 20:52:20 -0300

    Remove createToken da service de registro

--- back-end/src/routes/RegisterRoutes/Register.routes.js
+++ back-end/src/routes/RegisterRoutes/Register.routes.js
@@ -3,9 +3,11 @@ const rescue = require('express-rescue');
 const validateJoi = require('../../middlewares/validateJoi');
 const { validRegister } = require('../../schemas/validateRegister');
 const RegisterController = require('../../controller/Register.controller');
+const { authMiddleware } = require('../../middlewares/authMiddleware');
 
 const routes = express.Router();
 
 routes.post('/', validateJoi(validRegister), rescue(RegisterController.createRegister));
+routes.delete('/', authMiddleware,rescue(RegisterController.deleteRegister));
 
 module.exports = routes;
