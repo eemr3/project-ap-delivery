32a7ec5d5b7db23cfbf620e8718aec7f6a5e8e60
carlosdartora <carlosdalsoler@gmail.com>
2022-07-18 18:15:42 -0300

    feat(sales.route): adiciona toda o fluxo de criar venda

--- back-end/src/routes/SalesRoutes/Sales.routes.js
+++ back-end/src/routes/SalesRoutes/Sales.routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const rescue = require('express-rescue');
-// const validateJoi = require('../../middlewares/validateJoi');
-// const { validRegister } = require('../../schemas/validateRegister');
+const validateJoi = require('../../middlewares/validateJoi');
+const { validSales } = require('../../schemas/validateSales');
 const SalesController = require('../../controller/Sales.controller');
+const { authMiddleware } = require('../../middlewares/authMiddleware');
 
 const routes = express.Router();
 
-routes.post('/customer/orders/<id>', /*validateJoi()*/ rescue(SalesController.createSales));
+routes.post(
+  '/',
+  authMiddleware,
+  validateJoi(validSales),
+  rescue(SalesController.validateProducts),
+  rescue(SalesController.createSale),
+);
 
 module.exports = routes;
