11163aeefeb61aa590fc205f2ff79610e99506d1
JVSNG <joaovitorspala.js@gmail.com>
2022-07-18 22:44:06 -0300

    fix(lint): Conserta o erro de object destructuring na controller de register

--- back-end/src/controller/Register.controller.js
+++ back-end/src/controller/Register.controller.js
@@ -8,7 +8,7 @@ const createRegister = async (req, res) => {
 };
 
 const deleteRegister = async (req, res) => {
-    const id = req.params.id;
+    const { id } = req.params;
     const deletedUser = await RegisterService.deleteRegister(id);
 
     return res.status(200).json(deletedUser);
