ec089eaef435f5a810fd7b5810e350b4477568bb
carlosdartora <carlosdalsoler@gmail.com>
2022-07-19 16:52:41 -0300

    fix(admin/middeware): remove linha já verificada

--- back-end/src/middlewares/adminMiddleware.js
+++ back-end/src/middlewares/adminMiddleware.js
@@ -2,10 +2,11 @@ const { unauthorized } = require('../util/messageError');
 
 const adminMiddleware = (req, res, next) => {
   const user = req.data;
-  if (!user) return res.status(unauthorized.status).json({ message: unauthorized.message });
+
   if (user.role !== 'administrator') {
     return res.status(unauthorized.status).json({ message: unauthorized.message });
   }
+  
   next();
 };
 
