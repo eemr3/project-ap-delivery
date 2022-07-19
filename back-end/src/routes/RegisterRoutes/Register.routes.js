e35ed9efaac35c799f397ae6565bb644d4f2f3de
JVSNG <joaovitorspala.js@gmail.com>
2022-07-18 22:21:02 -0300

    feat(register route validation): Adiciona validação de administrador para a rota de delete do endpoint de registro

--- back-end/src/routes/RegisterRoutes/Register.routes.js
+++ back-end/src/routes/RegisterRoutes/Register.routes.js
@@ -4,10 +4,11 @@ const validateJoi = require('../../middlewares/validateJoi');
 const { validRegister } = require('../../schemas/validateRegister');
 const RegisterController = require('../../controller/Register.controller');
 const { authMiddleware } = require('../../middlewares/authMiddleware');
+const { adminMiddleware } = require('../../middlewares/adminMiddleware');
 
 const routes = express.Router();
 
 routes.post('/', validateJoi(validRegister), rescue(RegisterController.createRegister));
-routes.delete('/:id', rescue(RegisterController.deleteRegister));
+routes.delete('/:id', authMiddleware, adminMiddleware, rescue(RegisterController.deleteRegister));
 
 module.exports = routes;
