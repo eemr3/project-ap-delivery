78ea1aba8c94523c2cb7c7a1f8927c5cb262b849
carlosdartora <carlosdalsoler@gmail.com>
2022-07-19 10:21:26 -0300

    feat(sales.controller): adiciona getAll, getById, getAllFromCustomer e patchStatus

--- back-end/src/controller/Sales.controller.js
+++ back-end/src/controller/Sales.controller.js
@@ -1,5 +1,48 @@
 const SalesService = require('../services/Sales.service');
 const ProductService = require('../services/Product.service');
+const { unauthorized } = require('../util/messageError');
+
+const getAll = async (req, res) => {
+  const user = req.data;
+  
+  if (user.role === 'customer') {
+    res.status(unauthorized.status).send({ message: unauthorized.message });
+  }
+
+  const sales = await SalesService.getAll();
+
+  res.status(200).send(sales);
+};
+
+const getAllFromCustomer = async (req, res) => {
+  const user = req.data;
+
+  const sales = await SalesService.getAllFromCustomer(user.id);
+
+  res.status(200).send(sales);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const sale = await SalesService.getById(id);
+
+  res.status(200).send(sale);
+};
+
+const patchStatus = async (req, res) => {
+  const user = req.data;
+  const { status } = req.body;
+  const { id } = req.params;
+
+  if (user.role === 'customer') {
+    res.status(unauthorized.status).send({ message: unauthorized.message });
+  }
+
+  await SalesService.patchStatus(status, id);
+
+  return res.status(200).send();
+};
 
 const createSale = async (req, res) => {
   req.body.userId = req.data.id;
@@ -17,6 +60,10 @@ const validateProducts = async (req, res, next) => {
 };
 
 module.exports = {
+  getAll,
+  getAllFromCustomer,
+  getById,
+  patchStatus,
   createSale,
   validateProducts,
 };
