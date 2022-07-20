93b59c67384548367d6b2f2164a1358d8b57aee6
eemr3 <eemr3@yahoo.com.br>
2022-07-20 18:21:55 -0300

    feat(Navbar.jsx): adiciona condicional "administrator" para renderizar a navbar da tela de administrador

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const TITLE = 'Cevarápida';
 function Navbar() {
   const { name, role } = JSON.parse(localStorage.getItem('user'));
 
   if (role === 'seller') {
     return (
       <header className="header">
-        <h2>Cevarápida</h2>
+        <h2>{TITLE}</h2>
         <nav className="navbar">
           <Link
             to="/seller/orders"
@@ -31,9 +32,30 @@ function Navbar() {
     );
   }
 
+  if (role === 'administrator') {
+    return (
+      <header className="header">
+        <h2>{TITLE}</h2>
+        <nav className="navbar">
+          <h3 data-testid="customer_products__element-navbar-user-full-name">
+            { name }
+          </h3>
+          <Link
+            to="/login"
+            data-testid="customer_products__element-navbar-link-logout"
+            onClick={ () => localStorage.removeItem('user') }
+            className="exit-btn"
+          >
+            Sair
+          </Link>
+        </nav>
+      </header>
+    );
+  }
+
   return (
     <header className="header">
-      <h2>Cevarápida</h2>
+      <h2>{TITLE}</h2>
       <nav className="navbar">
         <Link
           to="/customer/products"
